6fae4d822319488c298491b0b9074fdd980af895
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-11-20 10:45:27 +0600

    second commit

--- main_page/urls.py
+++ main_page/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import manga_list_view, manga_list_detail_view
 
 urlpatterns = [
-    path('film_list/', manga_list_view),
-    path('film_list/<int:id>/', manga_list_detail_view),
+    path('manga_list/', manga_list_view),
+    path('manga_list/<int:id>/', manga_list_detail_view),
 ]
